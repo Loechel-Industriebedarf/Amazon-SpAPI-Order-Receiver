d4ca84c7467e103e0f1d4d145dfcb53e49f1f265
ADarkHero <maik.riedlsperger@gmail.com>
2023-08-28 10:48:17 +0200

    Added convert rules

--- convertReports.php
+++ convertReports.php
@@ -12,11 +12,52 @@
 
 		$data = str_getcsv($line, "\t");
 		
-		//MANIPULATION	
+		//Skip empty lines
 		if($data[0] != null){
-			($i == 0) ? $data[40] = "Nebenkosten" : $data[40] = $data[11] * 0.135;
+			//Skip additional headlines, if multiple reports exist
+			if($i == 0 || $data[0] != "order-id"){
+				//Manipulate data	
+				
+				
+				//Generate Headline
+				if($i == 0){
+					$data[40] = "Nebenkosten";
+					$data[41] = "Strasse";
+					$data[42] = "Firma 1";
+					$data[43] = "Firma 2";
+					$data[44] = "Firma 3";
+				}
+				else{
+					//Calculate costs
+					$data[40] = $data[11] * $data[9] * 0.135;
+					
+					//Sometimes the street is in ship-adress-1; sometimes it's in ship-address-2
+					//If ship-address-1 contains number the street is in ship-address-1
+					if (preg_match('~[0-9]+~', $data[17])) {
+						$data[41] = $data[17]; //ship-adress-1 in street
+						$data[42] = $data[16]; //recipient-name in Firma1
+						$data[43] = $data[18]; //ship-adress-2 in Firma2
+					}
+					else{
+						$data[41] = $data[18]; //ship-adress-2 in street
+						$data[42] = $data[17]; //ship-adress-1 Firma1
+						$data[43] = $data[16]; //recipient-name in Firma2
+					}
+					
+					//No difference between the two logics
+					$data[44] = $data[19]; //ship-adress-3 in Firma3
+					
+					//Check if buyer-company-name is set; If it's set, check for duplicates and add it to Firma1
+					if($data[34] != ""){
+						if($data[17] != $data[34] && $data[16] != $data[34]){ 
+							$data[42] = $data[42] . " - " . $data[34];
+						} 
+					}
+					
+				}
 			
-			array_push($orders, $data);
+				array_push($orders, $data);
+			}
 		}
 		
 		$i++;
@@ -36,7 +77,8 @@
 	$fp = fopen('../amazonOrder' . $currentDateTime->format('Y-m-d\TH-i-s') . '.csv', 'w');
 	
 	foreach ($orders as $order) {
-		fputcsv($fp, $order, ";");
+		$ord = str_replace('"', '', $order); //Remove " - Enventa doesn't like csvs with "
+		fputcsv($fp, $ord, ";");
 	}
 
 	fclose($fp);
